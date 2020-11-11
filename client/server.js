186a1db2b14d81fdb83c8e0a65c7cf2261f97c27
Λ B H I N Λ V <ixd.abhi@gmail.com>
2020-11-11 16:47:38 -0500

    started resthooks

--- client/server.js
+++ client/server.js
@@ -3,7 +3,7 @@ const express = require('express'),
     server = app.listen(1000)
 
 app.use(express.static('public'));
-console.log("server running...");
+console.log("server running... 1000");
 
 app.get("/", (req, res) => {
    
